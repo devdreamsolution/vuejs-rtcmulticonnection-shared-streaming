b9296a02f276994a94978ada4a6f709dd1602038
Devsolution <q3construction1@gmail.com>
2020-06-08 19:29:46 +0800

    Add base URL of API

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,9 @@
 // axios
 import axios from 'axios'
 
-const baseURL = ''
+const baseURL = 'http://127.0.0.1:8000'
 
 export default axios.create({
-  baseURL
+  baseURL: baseURL
   // You can add your headers here
 })
